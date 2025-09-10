85325c1a9c480f21e8aec862dc3241f749a4a6f3
Bencuska <rizb@kkszki.hu>
2025-09-10 09:24:36 +0200

    Feladat3 is ready

--- OOPalapok/Program.cs
+++ OOPalapok/Program.cs
@@ -11,7 +11,28 @@ namespace OOPalapok
         public string nev;
         public int kor;
 
-        public Szemely(string Nev, int Kor)
+        public string Nev
+        {
+            get { return nev; } 
+            set { nev = value; }
+        }
+
+        public int Kor
+        {
+            get { return kor; }
+            set 
+            { 
+                if (value < 0)
+                {
+                    kor = 0;
+                }
+                else
+                {
+                    kor = value; 
+                }
+            }
+        }
+        /*public Szemely(string Nev, int Kor)
         {
             nev = Nev;
             kor = Kor;
@@ -20,16 +41,22 @@ namespace OOPalapok
         public string Kiir()
         {
             return $"A tanulo neve: {nev} és {kor} éves";
-        }
+        }*/
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Szemely tanulo1 = new Szemely("Jani", 55);
+            Szemely tanulo1 = new Szemely();
+            tanulo1.Nev = "Gábor";
+            Console.WriteLine(tanulo1.Nev);
+            tanulo1.Kor = 23;
+            Console.WriteLine(tanulo1.Kor);
+
+            /*Szemely tanulo1 = new Szemely("Jani", 55);
             Console.WriteLine(tanulo1.Kiir());
             Szemely tanulo2 = new Szemely("Feri", 45);
-            Console.WriteLine(tanulo2.Kiir());
+            Console.WriteLine(tanulo2.Kiir());*/
         }
     }
 }
