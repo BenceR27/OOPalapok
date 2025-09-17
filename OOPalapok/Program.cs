d776ba0edc15d2b7d164f20a6814386543ecbef8
Bencuska <rizb@kkszki.hu>
2025-09-17 09:24:39 +0200

    Feladat9 is ready

--- OOPalapok/Program.cs
+++ OOPalapok/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Markup;
@@ -88,20 +89,42 @@ namespace OOPalapok
             Szemely tanulo1 = new Szemely();
             tanulo1.Nev = "Gábor";
             tanulo1.Kor = 23;
-
             Console.WriteLine(tanulo1);
+
             Console.WriteLine("---------------------");
 
             Bankszamla bankszamla1 = new Bankszamla();
             bankszamla1.Egyenleg = 23000;
             Console.WriteLine(bankszamla1.Egyenleg);
+
             Console.WriteLine("---------------------");
 
             Hallgato hallgato1 = new Hallgato();
             hallgato1.NeptunKod = "RIZZ27";
             Console.WriteLine(hallgato1.NeptunKod);
+
             Console.WriteLine("---------------------");
 
+            List<Hallgato> hallgatok = new List<Hallgato>();
+
+            for (int i = 0; i < 2; i++)
+            {
+                Hallgato hallgato = new Hallgato();
+                Console.Write($"Kérem a(z) {i + 1} hallgató nevét: ");
+                hallgato.Nev = Console.ReadLine();
+                Console.Write($"Kérem a(z) {i + 1} hallgató életkorát: ");
+                hallgato.Kor = int.Parse( Console.ReadLine() );
+                Console.Write($"Kérem a(z) {i + 1} hallgató neptun kódját: ");
+                hallgato.NeptunKod = Console.ReadLine();
+                hallgatok.Add( hallgato );
+            }
+            Console.WriteLine("Hallgatók neve: ");
+            foreach (var item in hallgatok)
+            {
+                Console.WriteLine(item.Nev);
+            }
+
+            Console.WriteLine("---------------------");
         }
     }
 }
