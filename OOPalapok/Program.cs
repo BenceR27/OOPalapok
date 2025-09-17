ddf7343543087be67826f692763ac28f1bc7fb40
Bencuska <rizb@kkszki.hu>
2025-09-17 08:08:15 +0200

    Feladat5 is ready

--- OOPalapok/Program.cs
+++ OOPalapok/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Markup;
 
 namespace OOPalapok
 {
@@ -28,6 +30,34 @@ namespace OOPalapok
             return $"A személy neve {nev}, életkora {kor}";
         }
     }
+
+    public class Bankszamla
+    {
+        private int egyenleg;
+
+        public int Egyenleg
+        {
+            get { return egyenleg; }
+            set 
+            { 
+                if(value >=  0)
+                egyenleg = value;
+                else
+                    Console.WriteLine("Az érték nem lehet negatív. ");
+            }
+        }
+
+        public void Betesz()
+        {
+
+        }
+        
+        public void Kivesz()
+        {
+
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
@@ -37,6 +67,10 @@ namespace OOPalapok
             tanulo1.Kor = 23;
 
             Console.WriteLine(tanulo1);
+
+            Bankszamla bankszamla1 = new Bankszamla();
+            bankszamla1.Egyenleg = -23000;
+            Console.WriteLine(bankszamla1.Egyenleg);
             
         }
     }
